2010d5a43c0b7b05cf1cbfda68f2bb14ef4f7582
Robert Turner <georob23@gmail.com>
2015-09-27 16:02:38 -0700

    Finalized documentation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix() is a funtion that creates an enhanced matrix object. 
+## It takes a matrix as input and stores that matrix object and the calculated 
+## Inverse of that matrix, and provides helper functions to initialize the 
+## cacheMatrix, get the matrix and its inverse, and set the matrix and inverse.
+## Initially, the Inverse is set to NULL.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(cache_matrix = matrix()) {
         matrix_inverse <- NULL
@@ -21,6 +23,7 @@ makeCacheMatrix <- function(cache_matrix = matrix()) {
         # Function to return the inverse of the matrix
         getInverse <- function() matrix_inverse
         
+        # Return list of functions
         list(setMatrix = setMatrix, getMatrix = getMatrix,
              setInverse = setInverse,
              getInverse = getInverse)
@@ -28,23 +31,32 @@ makeCacheMatrix <- function(cache_matrix = matrix()) {
 }
 
 
-## cacheSolve() takes a matrix 'input_matrix' and 
-## returns a matrix that is the inverse of 'input_matrix'
+## cacheSolve() takes a matrix and returns a matrix that is the 
+## inverse. It first gets the inverse for the input matrix from its
+## cacheMatrix object. It then checks that the cacheMatrix inverse is valid 
+## (not NULL) and that the input matrix is the same as the cacheMatrix matrix. 
+## If so, it returns the inverse; if not, it calculates the inverse and 
+## sets it to the cacheMatrix object.
 
-cacheSolve <- function(input_matrix, ...) {
+cacheSolve <- function(input_cache_matrix, ...) {
         
-        matrix_inverse <- input_matrix$getInverse()
+        # Get the matrix inverse from the cacheMatrix
+        matrix_inverse <- input_cache_matrix$getInverse()
 
-        if(!is.null(matrix_inverse) && input_matrix == input_matrix$getMatrix()) {
+        # Check that the inverse is not NULL and that the input cache matrix
+        # and the internal archived matrix in the cacheMatrix agree.
+        # If so, return the already cached matrix.
+        if(!is.null(matrix_inverse) && input_cache_matrix == input_cache_matrix$getMatrix()) {
                 message("Getting cached data . . .")
-                return(input_matrix)
+                return(matrix_inverse)
         }
-        cache_matrix <- input_matrix$getMatrix()
 
+        #  Create a temporary cacheMatrix object, calculate and set the inverse
+        cache_matrix <- input_cache_matrix$getMatrix()
         matrix_inverse <- solve(cache_matrix, ...)
-
         input_matrix$setInverse(matrix_inverse)
 
+        # Return Inverse
         matrix_inverse
         
 }
